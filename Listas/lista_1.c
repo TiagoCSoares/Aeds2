f6400d6c22f8a5c17c822a2b27bcc9a119cca775
TiagoCSoares <tiagocsoares22@gmail.com>
2023-09-25 16:21:45 -0300

    Removendo primeiro elemento e elementos intermediários

--- Listas/lista_1.c
+++ Listas/lista_1.c
@@ -12,12 +12,14 @@ int disponivel;
 void iniciaLista(Node *lista, int *comeco, int *disponivel) {
 
     for (int i = 0; i < TAM-1; i++) {
-        lista[i].info = 0;
+        lista[i].info = i + 1;
         lista[i].prox = i + 1;
     }
     lista[TAM-1].info = 0;
     lista[TAM-1].prox = -1;
-    *comeco = -1;
+    //*comeco = -1;
+    //*disponivel = 0;
+    *comeco = 0;
     *disponivel = 0;
 }
 
@@ -56,19 +58,24 @@ void adicionaNaLista(Node lista[], int *disponivel, int *comeco, int valor) {
 // l[fim].prox = atualq:q
 
 
-void removerDaLista(Node *lista, int *disponivel, int comeco, int valor) {
-    int atual = comeco;
+void removerDaLista(Node *lista, int *disponivel, int *comeco, int valor) {
+    int atual = *comeco;
     int anterior = atual;
     int fim = lista[atual].prox;
     int removido = 0;
-    if (comeco == -1) {
+    if (*comeco == -1) {
         printf("Não há elementos a serem removidos\n");
     } else {
     while(lista[fim].prox != -1) {
         fim = lista[fim].prox;
     }
         do {
-            if(lista[atual].info == valor) {
+            if(lista[atual].info == valor && atual == *comeco){
+                *comeco = lista[atual].prox;
+            } else if (lista[atual].info == valor && lista[atual].prox == -1)
+            {
+                *disponivel = anterior;
+            } else if (lista[atual].info == valor) {
                 lista[anterior].prox = lista[atual].prox;
             }
             anterior = atual;
@@ -76,28 +83,6 @@ void removerDaLista(Node *lista, int *disponivel, int comeco, int valor) {
         } while(lista[atual].prox != fim); 
     }
 }
-
-//void removeFimLista(Node lista[], int *disponivel, int *comeco) {
-//    if(*disponivel == -1){
-//        *disponivel = TAM -1;
-//        lista[*disponivel].info = 0;
-//    } else {
-//        lista[*disponivel].info = 0;
-//        *disponivel -= 1;
-//    }
-//}
-//
-//void removeComecoLista(Node lista[], int *disponivel, int *comeco) {
-//    lista[*comeco].info = 0;Removendo:
-//    if(*disponivel == -1) {
-//        realocaLista(lista, *disponivel, *comeco);
-//    }
-//
-//}
-//
-//void realocaLista(Node lista[], int *disponivel, int *comeco) {
-//    printf("fsafs");
-//}
 
 int main() {
     Node lista[TAM];
@@ -126,7 +111,7 @@ int main() {
         case 3:
             printf("Valor a ser removido\n");
             scanf("%d", &valor);
-            removerDaLista(lista, &disponivel, comeco, valor);
+            removerDaLista(lista, &disponivel, &comeco, valor);
             break;
         case 0:
             printf("Saindo do menu\n");
